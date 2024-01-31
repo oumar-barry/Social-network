2751b12965b7ea09379565ab6780cab640b6297f
oumar <alphaoumareclipse@gmail.com>
2024-01-31 23:23:55 +0100

    feat: follow/ unfollow user complete

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -40,7 +40,47 @@ const login = asyncHandler(async (req, res, next) => {
 	res.status(201).json({ data: user });
 });
 
+/**
+ * @route PUT /api/user/:userId/follow
+ * @desc follow or unfollow a user
+ * @access private
+ */
+const follow = asyncHandler(async (req, res, next) => {
+	let user = await User.findById(req.params.userId);
+	if (!user) {
+		return next(new ErrorResponse('user not found ', 404));
+	}
+
+	if (req.user._id == req.params.userId) {
+		return next(new ErrorResponse("You can't follow yourself ", 400));
+	}
+
+	const isFollowing = req.user.following.includes(user._id);
+	const pushOrPull = isFollowing ? '$pull' : '$push';
+	req.user = await User.findByIdAndUpdate(
+		req.user.id,
+		{
+			[pushOrPull]: { following: req.params.userId },
+		},
+		{ new: true }
+	);
+
+	user = await User.findByIdAndUpdate(
+		user._id,
+		{
+			[pushOrPull]: { followers: req.user._id },
+		},
+		{ new: true }
+	);
+
+	//Send notification later
+
+	//Only return the current logged in user later
+	res.status(200).json({ data: { user, me: req.user } });
+});
+
 module.exports = {
 	register,
 	login,
+	follow,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
 
-const { register, login } = require('../controllers/UserController');
+const { register, login, follow } = require('../controllers/UserController');
 router.post('/register', register);
 router.post('/login', login);
+router.put('/:userId/follow', protect, follow);
+
 module.exports = router;
